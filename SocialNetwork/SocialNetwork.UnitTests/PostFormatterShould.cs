131834b030c13ded5a943b12a307eebc6f3bdfd4
ifagarasan <raptor_dev@yahoo.com>
2016-07-05 09:53:27 +0100

    Removed unused files. Refactored PostFormatterShould

--- SocialNetwork/SocialNetwork.UnitTests/PostFormatterShould.cs
+++ SocialNetwork/SocialNetwork.UnitTests/PostFormatterShould.cs
@@ -8,18 +8,27 @@ namespace SocialNetwork.UnitTests
     [TestClass]
     public class PostFormatterShould
     {
+        Mock<ITimeOffsetCalculator> _timeOffsetCalculatorMock;
+        PostFormatter _postFormatter;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _timeOffsetCalculatorMock = new Mock<ITimeOffsetCalculator>();
+
+            _postFormatter = new PostFormatter(_timeOffsetCalculatorMock.Object);
+        }
+
         [TestMethod]
         public void Format()
         {
             var post = new Post("Alice", "I love the weather today", DateTime.Now);
 
-            var timeOffsetCalculatorMock = new Mock<ITimeOffsetCalculator>();
-            timeOffsetCalculatorMock.Setup(m => m.NowToDateOffset(It.IsAny<DateTime>())).Returns(new TimeSpan(0, -3, -1));
+            _timeOffsetCalculatorMock.Setup(m => m.NowToDateOffset(It.IsAny<DateTime>())).Returns(new TimeSpan(0, -3, -1));
 
-            var postFormatter = new PostFormatter(timeOffsetCalculatorMock.Object);
-            var message = postFormatter.Format(post);
+            var message = _postFormatter.Format(post);
 
-            timeOffsetCalculatorMock.Verify(m => m.NowToDateOffset(post.WrittenAt));
+            _timeOffsetCalculatorMock.Verify(m => m.NowToDateOffset(post.WrittenAt));
            
             Assert.AreEqual($"{post.Message} (4 minutes ago)", message);
         }
@@ -29,13 +38,11 @@ namespace SocialNetwork.UnitTests
         {
             var post = new Post("Alice", "I love the weather today", DateTime.Now);
 
-            var timeOffsetCalculatorMock = new Mock<ITimeOffsetCalculator>();
-            timeOffsetCalculatorMock.Setup(m => m.NowToDateOffset(It.IsAny<DateTime>())).Returns(new TimeSpan(0, -1, 0));
+            _timeOffsetCalculatorMock.Setup(m => m.NowToDateOffset(It.IsAny<DateTime>())).Returns(new TimeSpan(0, -1, 0));
 
-            var postFormatter = new PostFormatter(timeOffsetCalculatorMock.Object);
-            var message = postFormatter.Format(post);
+            var message = _postFormatter.Format(post);
 
-            timeOffsetCalculatorMock.Verify(m => m.NowToDateOffset(post.WrittenAt));
+            _timeOffsetCalculatorMock.Verify(m => m.NowToDateOffset(post.WrittenAt));
 
             Assert.AreEqual($"{post.Message} (1 minute ago)", message);
         }
