94ce328239e6595446620457f736bf0d351ca38c
ifagarasan <raptor_dev@yahoo.com>
2016-07-05 12:09:21 +0100

    Added test for command processor not defined. Moved test files under Command. Added DisplayWall command.

---
+++ SocialNetwork/SocialNetwork/Exceptions/CommandProcessorNotDefinedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SocialNetwork.Exceptions
+{
+    public class CommandProcessorNotDefinedException: Exception
+    {
+        public CommandProcessorNotDefinedException()
+        {
+        }
+
+        public CommandProcessorNotDefinedException(string message) : base(message)
+        {
+        }
+    }
+}
