4892fcfa38f27d0e4f5ae5908d74e398cfd49e8c
ifagarasan <raptor_dev@yahoo.com>
2016-07-06 14:59:48 +0100

    Added Console and actual Program.cs functionality

--- SocialNetwork/SocialNetwork/Program.cs
+++ SocialNetwork/SocialNetwork/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace SocialNetwork
+﻿using SocialNetwork.Action;
+using SocialNetwork.Action.Command;
+using SocialNetwork.Action.Command.Input;
+using SocialNetwork.Action.Format;
+using SocialNetwork.Infrastructure.Date;
+using SocialNetwork.Infrastructure.Format;
+using SocialNetwork.Infrastructure.Time;
+using SocialNetwork.Model;
+
+namespace SocialNetwork
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            
+            var dateProvider = new DateProvider();
+            var console = new Infrastructure.Console();
+            var socialEngine = new SocialEngine(new Repository(dateProvider),
+                new PostFormatter(new TimeOffsetCalculator(dateProvider), new TimeFormatter()));
+
+            var socialPlatform = new SocialPlatform(new InputRetriever(new InputBuilder(), console),
+                new CommandFactory(socialEngine, console));   
+
+            socialPlatform.Run();
         }
     }
 }
